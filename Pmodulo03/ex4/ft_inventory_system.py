23d8a459945e98f331a53d67b260781361da5c25
Asesor <Gabrieldavidortizj@gmail.com>
2026-02-15 10:40:06 +0100

    algo

---
+++ Pmodulo03/ex4/ft_inventory_system.py
@@ -0,0 +1,16 @@
+def inventory_system():
+
+    print("=== Player Inventory System ===")
+
+    players = {'Alice': {'weapon': {'weapon': 'sword', 'rare': 'rare', 'point': '500'}},
+               'bob': {'first_kill', 'level_10', 'boss_slayer', 'collector'}}
+
+    # alice = players['alice']
+    # bob = players['bob']
+
+
+"""
+def show_inventory():
+
+def Transaction():
+"""
